b8f0dd79573010480223cfa58a5c10f2ab7ff93e
Skyawesome73 <skyawesome73@gmail.com>
2025-11-03 13:53:14 -0600

    add index

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+export default {
+	/**
+	 * This is the standard fetch handler for a Cloudflare Worker
+	 *
+	 * @param request - The request submitted to the Worker from the client
+	 * @param env - The interface to reference bindings declared in wrangler.jsonc
+	 * @param ctx - The execution context of the Worker
+	 * @returns The response to be sent back to the client
+	 */
+	async fetch(request, env, ctx): Promise<Response> {
+		// Create a stub to open a communication channel with the Durable Object
+		// instance named "foo".
+		//
+		// Requests from all Workers to the Durable Object instance named "foo"
+		// will go to a single remote Durable Object instance.
+		let html = " \
+    <text>Hello</text> \
+    ";
+
+		return new Response(html);
+	},
+} satisfies ExportedHandler<Env>;
