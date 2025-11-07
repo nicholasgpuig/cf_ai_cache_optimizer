e2ea1df3be081de47285ca16c6f330e0382309f6
Skyawesome73 <skyawesome73@gmail.com>
2025-11-07 13:38:55 -0600

    restructure project

--- workers/jest.config.js
+++ workers/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	roots: ['<rootDir>/src'],
-	testMatch: ['**/__tests__/**/*.test.ts'],
+	testMatch: ['**/__tests__/*.test.ts'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 	collectCoverageFrom: [
 		'src/**/*.ts',
